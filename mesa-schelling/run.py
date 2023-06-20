65fef73268e7c29b71089fe7f577fa7df3309a1f
riverawesome <r.vaudrin@hotmail.com>
2023-06-20 09:15:36 +0200

    added sliders for grid hxw

--- mesa-schelling/run.py
+++ mesa-schelling/run.py
@@ -1,4 +1,4 @@
 from server import server
 
-server.port = 8888  # Set the port number as desired
+server.port = 8080  # Set the port number as desired
 server.launch(open_browser=True)
--- mesa-schelling/server.py
+++ mesa-schelling/server.py
@@ -26,19 +26,17 @@ def schelling_draw(agent):
         portrayal["stroke_color"] = "#000000"
     return portrayal
 
-
-canvas_element = mesa.visualization.CanvasGrid(schelling_draw, 20, 20, 500, 500)
-happy_chart = mesa.visualization.ChartModule([{"Label": "happy", "Color": "Black"}])
-
 model_params = {
-    "height": 20,
-    "width": 20,
-    # "height": mesa.visualization.Slider("Heigth", 20, 10, 200, 10),
-    # "width": mesa.visualization.Slider("Width", 20, 10, 200, 10),
+    "height": mesa.visualization.Slider("Grid height", 20, 10, 100, 10),
+    "width": mesa.visualization.Slider("Grid width", 20, 10, 100, 10),
     "density": mesa.visualization.Slider("Agent density", 0.8, 0.1, 1.0, 0.1),
     "minority_pc": mesa.visualization.Slider("Fraction minority", 0.2, 0.00, 1.0, 0.05),
     "homophily": mesa.visualization.Slider("Homophily", 3, 0, 8, 1),
 }
+
+canvas_element = mesa.visualization.CanvasGrid(schelling_draw, 100, 100, 1000, 1000)
+happy_chart = mesa.visualization.ChartModule([{"Label": "happy", "Color": "Black"}])
+
 
 server = mesa.visualization.ModularServer(
     Schelling,
